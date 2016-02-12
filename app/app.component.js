78be632ef8c4d4c8cd8d72e936eb5cc7592534b5
bassijtsma <btsijtsma@gmail.com>
2016-02-12 16:42:49 +0100

    added component files

--- app/app.component.js
+++ app/app.component.js
@@ -22,7 +22,7 @@ System.register(['angular2/core'], function(exports_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1>'
+                        template: '<h1>My 2nd Angular 2 App</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
