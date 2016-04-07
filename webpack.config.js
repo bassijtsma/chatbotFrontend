6157ec604c3ae95c6dcad98c6f9b16cb89051b0a
bastronaut <btsijtsma@gmail.com>
2016-04-07 13:23:01 +0200

    added css to webpackand local

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
@@ -11,12 +13,17 @@ module.exports = {
   ],
   output: {
     path: __dirname + "/dist",
-    filename: "index_bundle.js"
+    filename: "index_bundle.js",
+    chunkFilename: "chunk_dist.js"
   },
   module : {
     loaders: [
-      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"}
+      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"},
+      { test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,  loader: 'url-loader?limit=100000' },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig, new ExtractTextPlugin("style.css", {
+      allChunks: true
+  })]
 }
