b0a5345082d4d37bf08eae3b2fb99a6df8fd519a
bastronaut <btsijtsma@gmail.com>
2016-05-21 13:01:44 +0200

    added optionmessage component

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chatbotfrontend
 frontend to implement rules for chatbot
+
+Frontend to CRUD conversations that can take place with the [Facebook chatbot](https://github.com/bastronaut/facebookChatbot/) and the [Whatsapp chatbot](https://github.com/bastronaut/whatsappChatbot/). The API can be found [Here](https://github.com/bastronaut/chatbotAPI/).
---
+++ app/components/OptionMessage.js
@@ -0,0 +1,18 @@
+/*
+Think about how to create the guiding 'option' chat conversations,
+in which the chatbot returns options that you can choose from to lead the
+conversation instead of the person having to guess which way the conversation
+goes into
+*/
+
+var React = require('react');
+var PropTypes = React.PropTypes;
+var ConversationItemActions = require('../actions/ConversationItemActions');
+
+var OptionMessage = React.createClass({
+  render: function() {
+    return (null);
+  }
+})
+
+module.exports = OptionMessage;
